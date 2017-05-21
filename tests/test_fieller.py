c60be9b5803d62f546de539b6aa1611ba0bb2154
Remis Lape <r.lape@ucl.ac.uk>
2017-05-21 20:13:52 +0100

    test_fieller.py updated with corresponding fixes + some cleaning

--- tests/test_fieller.py
+++ tests/test_fieller.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-
 from fieller import Fieller
 import statistics_EJ as s
+
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+def test_zero_input():
+    # a, b, sa, sb, r, tval
+    f = Fieller(0, 1, 0, 0, 0, 0)
+
+    expected_output = {
+        'ratio' : 0.0,
+        'clower': 0.0,
+        'cupper': 0.0,
+        'dlow'  : 0.0,
+        'dhi'   : 0.0,
+        'appsd' : 0.0,
+        'applo' : 0.0,
+        'apphi' : 0.0,
+        'cvr'   : 0.0}
+        
+    for key in expected_output :
+        fieller_output = vars(f)[key]
+        expected = expected_output[key]
+        assert fieller_output == expected
 
 def test_regression_fieller():
     a, b = 14, 7 # Nominator and denominator
@@ -20,14 +41,14 @@ def test_regression_fieller():
     flr = Fieller(a, b, sa, sb, r, tval)
     print(flr)
     
-    assert math.isclose(flr.ratio, 2.00000, rel_tol=0.0001)
-    assert math.isclose(flr.g, 0.268164, rel_tol=0.001)
-    assert math.isclose(flr.clower, 1.051413, rel_tol=0.0001)
-    assert math.isclose(flr.cupper, 4.414296, rel_tol=0.0001)
-    assert math.isclose(flr.dlow, -0.948587, rel_tol=0.0001)
-    assert math.isclose(flr.dhi, 2.414296, rel_tol=0.0001)
-    assert math.isclose(flr.appsd, 0.714286, rel_tol=0.0001)
-    assert math.isclose(flr.cvr, 35.714286, rel_tol=0.0001)
-    assert math.isclose(flr.applo, 0.705385, rel_tol=0.0001)
-    assert math.isclose(flr.apphi, 3.294615, rel_tol=0.0001)
+    assert isclose(flr.ratio, 2.00000, rel_tol=0.0001)
+    assert isclose(flr.g, 0.268164, rel_tol=0.001)
+    assert isclose(flr.clower, 1.051413, rel_tol=0.0001)
+    assert isclose(flr.cupper, 4.414296, rel_tol=0.0001)
+    assert isclose(flr.dlow, -0.948587, rel_tol=0.0001)
+    assert isclose(flr.dhi, 2.414296, rel_tol=0.0001)
+    assert isclose(flr.appsd, 0.714286, rel_tol=0.0001)
+    assert isclose(flr.cvr, 35.714286, rel_tol=0.0001)
+    assert isclose(flr.applo, 0.705385, rel_tol=0.0001)
+    assert isclose(flr.apphi, 3.294615, rel_tol=0.0001)
     #assert 0 == 1
