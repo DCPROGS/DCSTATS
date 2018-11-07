5dc03b60522f5e78997f2c0d65a0ed663b89ddb7
aplested <a.plested@gmail.com>
2018-11-07 11:50:37 +0100

    refreshed TK GUI and animated DC GIF

--- GUI/DC_Stats_Tk.py
+++ GUI/DC_Stats_Tk.py
@@ -27,16 +27,15 @@ __date__ ="$30-Apr-2009 14:51:10$"
 
 class DCP:
     def __init__(self, master):
-        self.master = master
-        frame = Frame(self.master)
+        frame = Frame(master)
         frame.config(background="#dcdcdc") #well, it has to be, right?
         #frame.pack()
-        self.master.title('DC Stats for Mac v. 0.4')    #   Main frame title
-        self.master.config(background="#dcdcdc")
+        master.title('DC Stats for Mac v. 0.4')    #   Main frame title
+        master.config(background="#dcdcdc")
         #master.geometry('450x480')
-        menubar = Menu(self.master)
+        menubar = Menu(master)
         
-        statmenu = Menu(menubar,tearoff=0)        
+        statmenu = Menu(menubar,tearoff=0)
         statmenu.add_command(label="Fieller", command=self.on_fieller)
         
         statmenu.rantest = Menu(statmenu)
@@ -48,49 +47,71 @@ class DCP:
         statmenu.add_command(label="Quit", command=master.quit)
         
         menubar.add_cascade(label="Statistical Tests", menu=statmenu)
-        self.master.config(menu=menubar)
+        master.config(menu=menubar)
         lframe = LabelFrame(master, width = 450, text="Welcome to DC's statistics tools", background="#dcdcdc")
         
-        Label(lframe, text="\nPlease select a test to run:", background="#dcdcdc").pack()
-        b2 = Button(lframe, text="Randomisation test : Continuous data", command=self.on_rantest_continuous, highlightbackground="#dcdcdc")
-        b3 = Button(lframe, text="Randomisation test : Binomial data", command=self.on_rantest_binomial, highlightbackground="#dcdcdc")
-        b4 = Button(lframe, text="Fieller's theorem for SD of a ratio", command=self.on_fieller, highlightbackground="#dcdcdc")
+        Label(lframe, text="\nPlease select a test to run:", background="#dcdcdc").pack(anchor=W)
+        b2 = Button(lframe, text="Randomisation test : Continuous data", width=30, pady=8, command=self.on_rantest_continuous, highlightbackground="#dcdcdc")
+        b3 = Button(lframe, text="Randomisation test : Binomial data", width=30, pady=8, command=self.on_rantest_binomial, highlightbackground="#dcdcdc")
+        b4 = Button(lframe, text="Fieller's theorem for SD of a ratio", width=30, pady=8, command=self.on_fieller, highlightbackground="#dcdcdc")
         b2.pack()
         b3.pack()
         b4.pack()
         
         Label(lframe, text="", background="#dcdcdc").pack()
-        #FIXME -animate the gif: http://pyinmyeye.blogspot.de/2012/08/tkinter-animated-labels-demo.html
-        picture = PhotoImage(file="GUI/dca2.gif")
-        dcpic = Label(lframe, image=picture, pady=60)
-        dcpic.picture = picture
-        dcpic.pack()
+        picture = PhotoImage(file="TkGUI/dca2.gif")
+        dcpic = Label(lframe, image=picture, pady=8)
+        dcpic.image = picture
+        dcpic.pack(fill=X)
+        dcpic.frame = 0
+        frame.after(500, self._play_gif, frame, dcpic, 500)
         
-        Label(lframe, text="David Colquhoun", background="#dcdcdc").pack()
+        Label(lframe, text="David Colquhoun", background="#dcdcdc", pady=5, font=("Helvetica", 12)).pack()
         lframe.pack()
         frame.pack()
-        version = Message(frame, width=400, text="\n\nPython version: " + sys.version, background="#dcdcdc", font=("Helvetica", 10), pady=5)
-        version.pack()
+        
+        b5 = Button(frame, text="Quit", command=master.quit, width=10, highlightbackground="#dcdcdc", pady=8)
+        b5.pack(anchor=E)
+        
+        version = Message(frame, width=350, text="https://github.com/aplested/DC-Stats\n\nPython version: " + sys.version, background="#dcdcdc", font=("Helvetica", 10))
+        version.pack(side=BOTTOM)
+        
         
         if platform() == 'Darwin':
-            print ("Trying to force window to the front on Mac OSX")    
+            print ("Trying to force window to the front on Mac OSX")
             try:
                 system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
             except:
                 system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python2.7" to true' ''')
-  
+
+def _play_gif(self, frame, w, interval):
+    # animates the GIF by rotating through
+    # the GIF animation frames
+    # modified from http://pyinmyeye.blogspot.de/2012/08/tkinter-animated-labels-demo.html
+    try:
+        opt = "GIF -index {}".format(w.frame)
+        w.image.configure(format=opt)
+        except TclError:
+            w.frame = 0
+            self._play_gif(frame, w, interval)
+            return
+        
+        w.frame += 1
+        
+        frame.after(interval, self._play_gif, frame, w, interval)
+    
     def on_fieller(self):
-        f = Toplevel(self.master)
+        f = Toplevel(master)
         FrameFieller(f)
 
-    def on_rantest_binomial(self):
-        rb = Toplevel(self.master)
-        FrameRantestBinomial(rb)
-
+def on_rantest_binomial(self):
+    rb = Toplevel(master)
+    FrameRantestBinomial(rb)
+    
     def on_rantest_continuous(self):
-        rc = Toplevel(self.master)
+        rc = Toplevel(master)
         FrameRantestContinuous(rc)
-
+    
     def on_help():
         pass
 
