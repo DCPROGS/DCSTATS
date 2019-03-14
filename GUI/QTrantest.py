b48b2bc32092a3b9db4871738ed8cda0412b2043
remislp <r.lape@ucl.ac.uk>
2019-03-14 12:52:53 +0000

    qt gui: Excel sheet selection added

--- GUI/QTrantest.py
+++ GUI/QTrantest.py
@@ -212,7 +212,10 @@ class rantestQT(QDialog):
     def load_data_from_Excel(self):
         #TODO: currently loads only firs two columns. Allow multiple column load.
         xl = pd.ExcelFile(self.filename)
-        dt = xl.parse(0)
+        dialog = ExcelSheetDlg(xl.sheet_names, self)
+        if dialog.exec_():
+            xlssheet = dialog.returnSheet()
+        dt = xl.parse(xlssheet)
         self.X = dt.iloc[:,0].dropna().values.tolist()
         self.Y = dt.iloc[:,1].dropna().values.tolist()
         self.get_basic_statistics()
@@ -268,3 +271,47 @@ class rantestQT(QDialog):
         movie.start()
         return movie_screen
 #######   TAB 1: WELCOME!  END   ############
+
+
+class ExcelSheetDlg(QDialog):
+    """
+    Dialog to choose Excel sheet to load.
+    """
+    def __init__(self, sheetlist, parent=None):
+        super(ExcelSheetDlg, self).__init__(parent)
+        self.sheet = ''
+        self.List = QListWidget()
+        self.List.addItems(sheetlist)
+        self.List.itemSelectionChanged.connect(self.sheetSelected)
+        layout1 = QHBoxLayout()
+        layout1.addWidget(self.List)
+        layout2 = QVBoxLayout()
+        layout2.addLayout(layout1)
+        layout2.addWidget(ok_cancel_button(self))
+        self.setLayout(layout2)
+        self.resize(200, 300)
+        self.setWindowTitle("Choose Excel sheet to load...")
+
+    def sheetSelected(self):
+        """
+        Get selected sheet name.
+        """
+        self.sheet = self.List.currentRow()
+
+    def returnSheet(self):
+        """
+        Return selected sheet name.
+        """
+        return self.sheet
+
+def ok_cancel_button(parent):
+    buttonBox = QDialogButtonBox(QDialogButtonBox.Ok|QDialogButtonBox.Cancel)
+    buttonBox.button(QDialogButtonBox.Ok).setDefault(True)
+    buttonBox.accepted.connect(parent.accept)
+    buttonBox.rejected.connect(parent.reject)
+    # Following is for pyqt4
+    #self.connect(buttonBox, SIGNAL("accepted()"),
+    #     self, SLOT("accept()"))
+    #self.connect(buttonBox, SIGNAL("rejected()"),
+    #     self, SLOT("reject()"))
+    return buttonBox
